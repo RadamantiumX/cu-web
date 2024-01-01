6ac48bf743d1a1aaa7f9ecb5fc45c50006fc3669
RadamantiumX <eduardo2502@gmail.com>
2024-01-01 01:21:38 -0300

    another change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,24 +2,38 @@ import Logo from '../assets/no-background.png'
 import './styles-navbar.css'
 
 export default function NavBar() {
-    return(
+    return (
         <section className='container-navbar'>
-         <nav className="navbar container">
+            <nav className="navbar container">
 
-            <a href='#' className='navbar__logo'>
-                <img src={Logo} alt="Conn Urban Logo" title='Brand of the Conn Urban'/>
-            </a>
+                <a href='#' className='navbar__logo'>
+                    <img src={Logo} alt="Conn Urban Logo" title='Brand of the Conn Urban' />
+                </a>
 
-            <div className='navbar__nav'>
-               <ul className='nav__list'>
-                  <li className='nav__Link'>
-                    <a href="#">Ingresar</a>
-                  </li>
-               </ul>
-        
-            </div>
-            <a href="#" className='button button-primary button-saber-mas'>¡Saber más!</a>
-         </nav>
+                <div className='navbar__nav'>
+                    <ul className='nav__list'>
+                        <li className='nav__link'>
+                            <a href="#"><i style={{ marginRight: '5px' }} className="fa-solid fa-user"></i> Ingresar</a>
+                        </li>
+                    </ul>
+                    <a href="#" className='button button-primary button-saber-mas'>¡Saber más!</a>
+
+                    {/* Responsive Button */}
+                    <button className='navbar__toggle-btn' style={{ marginLeft: '7px' }}>
+                    <i className="fa-solid fa-ellipsis-vertical fa-lg"></i>
+                    </button>
+                    <div className='navbar__mobile-menu'>
+                    <ul className='nav__list'>
+                        <li className='nav__link-mobile'>
+                            <a href="#"><i style={{ marginRight: '5px' }} className="fa-solid fa-user"></i> Ingresar</a>
+                        </li>
+                    </ul>
+                    </div>
+                    {/* Responsive Button */}
+
+                </div>
+
+            </nav>
         </section>
     )
 }
